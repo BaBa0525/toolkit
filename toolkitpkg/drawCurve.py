90692574516e5d202df976ae75f1cd10137bf405
alan910127 <alan469357@gmail.com>
2022-08-13 02:14:31 +0800

    Add Precision-Recall Curve

--- toolkitpkg/drawCurve.py
+++ toolkitpkg/drawCurve.py
@@ -1,14 +1,15 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import precision_recall_curve
 
 CATEGORY = 'border'
 INPUT_FILE = f'./testing-data/{CATEGORY}.csv'
-OUTPUT_FILE = f'./output/{CATEGORY}.png'
+OUTPUT_ROC = f'./output/{CATEGORY}-roc.png'
+OUTPUT_PR = f'./output/{CATEGORY}-pr.png'
 
-def main():
-    df = pd.read_csv(INPUT_FILE)
+def drawROC(inputFile: str, outputFile: str):
+    df = pd.read_csv(inputFile)
 
     y_test, probas = df['actual'], df['predict']
 
@@ -28,8 +29,37 @@ def main():
     plt.title('Receiver operating characteristic')
     plt.legend(loc="lower right")
 
+    plt.savefig(outputFile)
     plt.show()
-    plt.savefig(OUTPUT_FILE)
+
+
+def drawPR(inputFile: str, outputFile: str):
+    df = pd.read_csv(inputFile)
+
+    y_test, probas = df['actual'], df['predict']
+
+    precision, recall, _ = precision_recall_curve(y_test, probas)
+
+    plt.clf()
+    plt.plot(recall, precision, label='PR curve')
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision-Recall Curve')
+    plt.legend(loc="lower right")
+
+    plt.savefig(outputFile)
+    plt.show()
+
+
+
+
+def main():
+
+    drawROC(INPUT_FILE, OUTPUT_ROC)
+    drawPR(INPUT_FILE, OUTPUT_PR)
 
 
 if __name__ == "__main__":
