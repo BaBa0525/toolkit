957206190f2282a74235adb16d2b14efd0d76a55
alan910127 <alan469357@gmail.com>
2022-08-19 17:14:50 +0800

    Add docstring

--- text_detection/helpers.py
+++ text_detection/helpers.py
@@ -3,18 +3,33 @@ from google.cloud import vision
 from statistics import mean
 
 
+def filename_to_image(filename: str):
+    """Return a vision.Image instance according to filename."""
+
+    with open(filename, mode="rb") as image_file:
+        content = image_file.read()
+
+    return vision.Image(content=content)
+
+
 def get_text_of_word(word: vision.Word) -> str:
+    """Return the text in a vision.Word instance."""
+
     return "".join(symbol.text for symbol in word.symbols)
 
 
 def mean_of_bounding_box(boundingBox: vision.BoundingPoly) -> int:
+    """Return the mean of y-coordinates of a bounding box rounded to nearest integer."""
+
     yCoords = [vertex.y for vertex in boundingBox.vertices]
     return round(mean(yCoords))
 
 
-def to_regex_string(oldString: str) -> str:
+def to_regex_string(string: str) -> str:
+    """Convert string to a valid regex string by replacing all preserved symbols."""
+
     return (
-        oldString.replace("$", r"\$")
+        string.replace("$", r"\$")
         .replace("^", r"\^")
         .replace("+", r"\+")
         .replace("(", r"\(")
@@ -23,4 +38,6 @@ def to_regex_string(oldString: str) -> str:
 
 
 def to_regex_string_list(oldList: "list[str]") -> "list[str]":
+    """Convert all strings in list to a valid regex string."""
+
     return [to_regex_string(string) for string in oldList]
--- utils/decorators.py
+++ utils/decorators.py
@@ -3,6 +3,9 @@ def static_vars(**kwargs):
     Adding static-variable-like attributes to functions.
 
     Those "static variables" can be accessed through `funcName.attrName`.
+
+    Args:
+        **kwargs: Arbitrary names with the initial value as needed.
     """
 
     def wrapper(func):
