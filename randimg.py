ba88e8c01c28864fd347eced7e32f16382d27376
alan910127 <alan469357@gmail.com>
2022-08-22 01:55:09 +0800

    Sort result output

--- randimg.py
+++ randimg.py
@@ -1,23 +1,45 @@
 import os
 import random
-import argparse
+
+
+ROOT_DIRECTORY = ""
+NUMBER_OF_IMAGES = 100
+
+
+def get_user_id_index(routes: "list[str]") -> int:
+    for index, route in enumerate(routes):
+        if "U" in route:
+            return index
+
+
+def sample_sorting_key(sample: str) -> "tuple[int, int, str]":
+    path = os.path.normpath(sample)
+    routes = path.split(os.path.sep)
+
+    userIdIndex = get_user_id_index(routes)
+
+    userId, folderId, imageName = routes[userIdIndex:]
+
+    # userId: 'Uxxx'
+    return int(userId[1:]), int(folderId), imageName
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("k", type=int, help="number of samples")
-    args = parser.parse_args()
+    if not os.path.isdir(ROOT_DIRECTORY):
+        raise NotADirectoryError(f"{ROOT_DIRECTORY!r} is not a directory.")
 
     allImages: "list[str]" = []
 
-    for root, _, files in os.walk("/mnt/d/IG"):
+    for root, _, files in os.walk(ROOT_DIRECTORY):
         allImages.extend(os.path.join(root, file) for file in files)
 
-    filteredImages = filter(lambda x: "crop" not in x, allImages)
+    filteredImages = list(filter(lambda x: "crop" not in x, allImages))
 
-    samples = random.sample(range(len(filteredImages)), k=args.k)
+    samples = random.sample(filteredImages, k=NUMBER_OF_IMAGES)
 
-    print(*[filteredImages[sample] for sample in samples], sep="\n")
+    results = sorted(samples, key=sample_sorting_key)
+
+    print("\n".join(results))
 
 
 if __name__ == "__main__":
